80a8d1d07d6640a9ff902738b1c48a73b877c8be
Jai <jaikuraria@gmail.com>
2019-01-11 12:24:09 +0530

    First Draft

---
+++ src/main/java/com/example/swagger/SwaggerSpringFoxApplication.java
@@ -0,0 +1,14 @@
+package com.example.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerSpringFoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerSpringFoxApplication.class, args);
+	}
+
+}
+
